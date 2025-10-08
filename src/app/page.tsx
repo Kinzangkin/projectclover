d93a533c7915ee620d92a93da79abf3ca2024c8a
Kinzangkin <franclinpondaag@gmail.com>
2025-10-08 20:01:37 +0800

    tambahin iklan hehe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+"use client";
 import Navbar from "../../components/Navbar";
 import Hero from "../../components/Hero";
 import About from "../../components/About";
 import Member from "../../components/Member";
 import Dedication from "../../components/Dedication";
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -13,6 +15,11 @@ export default function Home() {
       <div id="about"><About /></div>
       <div id="member"><Member /></div>
       <div id="dedication"><Dedication /></div>
+      <Script
+        src="//pl27807206.revenuecpmgate.com/05/b0/5e/05b05e893dd5ca87003b50c59a362125.js"
+        strategy="afterInteractive"
+        onError={(e) => console.error('Script load error', e)}
+      />
     </div>
     </>
   );
