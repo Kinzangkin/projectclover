ea3ed3602474019e61beb29646c92df7347b9003
Kinzangkin <franclinpondaag@gmail.com>
2025-11-12 20:40:48 +0800

    Remove external script from Home component
    Removed external script loading from the Home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Hero from "../../components/Hero";
 import About from "../../components/About";
 import Member from "../../components/Member";
 import Dedication from "../../components/Dedication";
-import Script from "next/script";
 
 export default function Home() {
   return (
@@ -15,11 +14,6 @@ export default function Home() {
       <div id="about"><About /></div>
       <div id="member"><Member /></div>
       <div id="dedication"><Dedication /></div>
-      <Script
-        src="//pl27807206.revenuecpmgate.com/05/b0/5e/05b05e893dd5ca87003b50c59a362125.js"
-        strategy="afterInteractive"
-        onError={(e) => console.error('Script load error', e)}
-      />
     </div>
     </>
   );
